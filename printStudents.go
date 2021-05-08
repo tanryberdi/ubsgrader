ff740651ef4d9dfb47e0046bd344b16d42b89059
moogod <tanryberdi@gmail.com>
2021-05-08 18:25:25 +0500

    deleted studentcounter variable

--- printStudents.go
+++ printStudents.go
@@ -6,20 +6,17 @@ import (
 
 func printStudents() {
 
-	countStudent := 0
-
 	for i := 1; i < 99; i++ { // for entrance year
 		for j := 1; j < 29; j++ { // for class letter
 			for k := 1; k < 49; k++ { // for order id student
 				for l := 0; l < 2; l++ { // for booklet number
 					if hasStudent[i][j][k][l] {
 						fmt.Println("Student ", i, " ", j, " ", k, " ", l)
-						countStudent++
 					}
 				}
 			}
 		}
 	}
 
-	fmt.Println("Number of Students = ", countStudent)
+	fmt.Println("Number of Students = ", nline)
 }
