7b70cd95fce279428a7a3e06b35d8dd2d5748c2f
moogod <tanryberdi@gmail.com>
2021-05-21 14:55:28 +0500

    Excelize library removed

--- printStudents.go
+++ printStudents.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/tidwall/gjson"
 )
 
@@ -57,16 +56,21 @@ func printStudents() {
 	for i := 1; i < 99; i++ { // for entrance year
 		if hasEntranceYear[i] {
 
-			f := excelize.NewFile()
-			f.SetCellValue("Sheet1", "A1", "T/b")
-			f.SetCellValue("Sheet1", "B1", "Kod")
-			f.SetCellValue("Sheet1", "C1", "Synpy")
-			f.SetCellValue("Sheet1", "D1", "True")
-			f.SetCellValue("Sheet1", "E1", "False")
-			f.SetCellValue("Sheet1", "F1", "Point")
+			/*
+				f := excelize.NewFile()
+				f.SetCellValue("Sheet1", "A1", "T/b")
+				f.SetCellValue("Sheet1", "B1", "Kod")
+				f.SetCellValue("Sheet1", "C1", "Synpy")
+				f.SetCellValue("Sheet1", "D1", "True")
+				f.SetCellValue("Sheet1", "E1", "False")
+				f.SetCellValue("Sheet1", "F1", "Point")
 
-			ch := "F1"
-			fmt.Println(string(ch[0] + 1))
+				ch := "G1"
+				fmt.Println(string(ch[0] + 1))
+				for j:=0; j<numOfSubjects[i]; j++ {
+					f.SetCellValue("Sheet1",)
+				}
+			*/
 
 			for j := 1; j < 29; j++ { // for class letter
 				for k := 1; k < 49; k++ { // for order id student
@@ -93,8 +97,10 @@ func printStudents() {
 				}
 			}
 
-			err := f.SaveAs("Result" + strconv.Itoa(i) + ".xlsx")
-			check(err)
+			/*
+				err := f.SaveAs("Result" + strconv.Itoa(i) + ".xlsx")
+				check(err)
+			*/
 
 		}
 	}
