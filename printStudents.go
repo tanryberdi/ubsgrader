de7f6bdc4c6a15a8933f1b7c489d907d262f806a
moogod <tanryberdi@gmail.com>
2021-05-25 14:36:46 +0500

    All informations are inserted to the db

--- printStudents.go
+++ printStudents.go
@@ -50,6 +50,7 @@ func getBooklet(entranceY int, bookN int) string {
 
 func printStudents() {
 
+	q := 0 // Number of students
 	var (
 		studentID  int
 		entranceY  int
@@ -64,43 +65,75 @@ func printStudents() {
 		err := rows.Scan(&studentID, &entranceY, &classL, &orderIdStd, &bookletN)
 		check(err)
 		fmt.Println(studentID, entranceY, classL, orderIdStd, bookletN)
+
+		q++
+		fmt.Println(q, "-> Student ", entranceY, " ", classL, " ", orderIdStd, " ", bookletN, " ",
+			conditionsOfAllStudents[entranceY][classL][orderIdStd][bookletN][1],
+			conditionsOfAllStudents[entranceY][classL][orderIdStd][bookletN][0],
+			conditionsOfAllStudents[entranceY][classL][orderIdStd][bookletN][2],
+			points[entranceY][classL][orderIdStd][bookletN])
+
+		for i := 0; i < numOfSubjects[entranceY]; i++ {
+			stmt, err := db.Prepare("INSERT INTO subjects(student_id, subject_id, nquestion, tr, fls, unsgnd, point) VALUES(?,?,?,?,?,?,?)")
+			check(err)
+			tr, fls, unsgnd, point := calcTrFl(entranceY, classL, orderIdStd, bookletN, subjectsLimit[entranceY][bookletN][i][0], subjectsLimit[entranceY][bookletN][i][1])
+			_, err = stmt.Exec(studentID, i, subjectsLimit[entranceY][bookletN][i][1]-subjectsLimit[entranceY][bookletN][i][0]+1,
+				tr, fls, unsgnd, point)
+			check(err)
+
+			// Print out section
+
+			fmt.Println(i, "--", subjectsLimit[entranceY][bookletN][i][0], "-->", subjectsLimit[entranceY][bookletN][i][1], ":",
+				table[entranceY][classL][orderIdStd][bookletN][subjectsLimit[entranceY][bookletN][i][0]:subjectsLimit[entranceY][bookletN][i][1]+1])
+			fmt.Print("---> ")
+
+			subject := gjson.Get(SubjectsJSON, getBooklet(entranceY, classL)+"."+strconv.Itoa(i)+".Subject")
+			fmt.Print(subject.String(), " ")
+
+			fmt.Println(calcTrFl(entranceY, classL, orderIdStd, bookletN, subjectsLimit[entranceY][bookletN][i][0], subjectsLimit[entranceY][bookletN][i][1]))
+		}
+		fmt.Println("-----------------------------")
+
 	}
 	err = rows.Err()
 	check(err)
 
-	q := 0 // Number of student
-	// Print results of students according to the entrance year
-	fmt.Println("Print results of students according to the entrance year")
-	for i := 1; i < 99; i++ { // for entrance year
-		if hasEntranceYear[i] {
+	/*
+		This section is a full print out all materials for testing...
+		q = 0 // Number of student
+		// Print results of students according to the entrance year
+		fmt.Println("Print results of students according to the entrance year")
+		for i := 1; i < 99; i++ { // for entrance year
+			if hasEntranceYear[i] {
 
-			for j := 1; j < 29; j++ { // for class letter
-				for k := 1; k < 49; k++ { // for order id student
-					for l := 0; l < 2; l++ { // for booklet number
+				for j := 1; j < 29; j++ { // for class letter
+					for k := 1; k < 49; k++ { // for order id student
+						for l := 0; l < 2; l++ { // for booklet number
 
-						if hasStudent[i][j][k][l] {
-							q++
-							fmt.Println(q, "-> Student ", i, " ", j, " ", k, " ", l, " ", conditionsOfAllStudents[i][j][k][l][1], conditionsOfAllStudents[i][j][k][l][0], conditionsOfAllStudents[i][j][k][l][2], points[i][j][k][l])
+							if hasStudent[i][j][k][l] {
+								q++
+								fmt.Println(q, "-> Student ", i, " ", j, " ", k, " ", l, " ", conditionsOfAllStudents[i][j][k][l][1], conditionsOfAllStudents[i][j][k][l][0], conditionsOfAllStudents[i][j][k][l][2], points[i][j][k][l])
 
-							for ii := 0; ii < numOfSubjects[i]; ii++ {
-								fmt.Println(ii, "--", subjectsLimit[i][l][ii][0], "-->", subjectsLimit[i][l][ii][1], ":", table[i][j][k][l][subjectsLimit[i][l][ii][0]:subjectsLimit[i][l][ii][1]+1])
-								fmt.Print("---> ")
+								for ii := 0; ii < numOfSubjects[i]; ii++ {
+									fmt.Println(ii, "--", subjectsLimit[i][l][ii][0], "-->", subjectsLimit[i][l][ii][1], ":", table[i][j][k][l][subjectsLimit[i][l][ii][0]:subjectsLimit[i][l][ii][1]+1])
+									fmt.Print("---> ")
 
-								subject := gjson.Get(SubjectsJSON, getBooklet(i, l)+"."+strconv.Itoa(ii)+".Subject")
-								fmt.Print(subject.String(), " ")
+									subject := gjson.Get(SubjectsJSON, getBooklet(i, l)+"."+strconv.Itoa(ii)+".Subject")
+									fmt.Print(subject.String(), " ")
 
-								fmt.Println(calcTrFl(i, j, k, l, subjectsLimit[i][l][ii][0], subjectsLimit[i][l][ii][1]))
+									fmt.Println(calcTrFl(i, j, k, l, subjectsLimit[i][l][ii][0], subjectsLimit[i][l][ii][1]))
+
+								}
+								fmt.Println("-----------------------------")
 
 							}
-							fmt.Println("-----------------------------")
-
 						}
 					}
 				}
 			}
 		}
-	}
-	fmt.Println("printing ended...")
+		fmt.Println("printing ended...")
+	*/
 
 	fmt.Println("Number of Students = ", nline)
 }
